69d064f1c21fe40a191a9bb2e07536bf165043fc
mo-alabsi <absimoda1@gmail.com>
2023-09-12 20:32:25 +0200

    import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 // import {cart as mycart} from '../data/cart.js'; avoid conflict with as someting
 // const cart =[];
